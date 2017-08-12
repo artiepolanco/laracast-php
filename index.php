16c089b6cbf39929f4a2edcd2124a79fed783012
Arturo Polanco <artiepolanco@gmail.com>
2017-08-12 15:33:40 -0400

    created Task class and dd function. Require does not seem to be working right now

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
 
-echo 'Hello World';
+require 'functions.php';
+
+
+
+class Task {
+  protected $description;
+  protected $completed = false;
+
+  public function __construct($description)
+  {
+    $this->description = $description;
+  }
+}
+
+$task = new Task('Go to the store');
+
+dd($task);
+
+
+
+require 'index.view.php';
